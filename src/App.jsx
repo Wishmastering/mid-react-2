52a14588876599a4fe067e682ffe5e8b31a131aa
Wishmastering <charsdb@gmail.com>
2023-07-28 10:25:55 -0600

    coded day

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ export default function App() {
 
   const handleSubmit = (name, email) => {
     setProfileData((prev) => [...prev, { name, email }]);
-    console.log("Submit Handled");
+    console.log("Submit Handled.");
   };
 
   return (
